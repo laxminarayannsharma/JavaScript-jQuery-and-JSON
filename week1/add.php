432148ac4c98bdd62f5b63436dfb918cb8514084
Laxminarayan Sharma <42807897+laxminarayannsharma@users.noreply.github.com>
2020-05-28 19:58:44 +0530

    Update add.php

--- week1/add.php
+++ week1/add.php
@@ -37,7 +37,7 @@ else{
 <html>
 <head>
     <?php require_once "bootstrap.php"; ?>
-    <title>Ziang Cui 's Login Page</title>
+    <title>Laxminarayan Naresh Sharma Login Page</title>
 </head>
 <body>
 <div class="container">
